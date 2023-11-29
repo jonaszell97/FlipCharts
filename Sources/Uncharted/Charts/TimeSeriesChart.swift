3a883824dfb0cc794008d8006a956e2fd716cb91
Jonas Zell <zell.jonas97@gmail.com>
2023-11-29 12:21:40 +0100

    add MultipleTimeSeriesView

--- Sources/Uncharted/Charts/TimeSeriesChart.swift
+++ Sources/Uncharted/Charts/TimeSeriesChart.swift
@@ -272,6 +272,84 @@ public struct TimeSeriesData {
     }
 }
 
+/// Create chart data for the currently selected time interval.
+fileprivate func getTimeSeriesChartData(source: TimeSeriesDataSource, for resolution: TimeSeriesScope) -> TimeSeriesData {
+    let interval = source.interval
+    let (dates, valueInterval) = resolution.dates(in: interval)
+    let values = (0..<(dates.count-1))
+        .map { (i: Int) -> (Int, Double?) in
+            guard let currentDate = dates[i] else {
+                return (i, nil)
+            }
+            
+            var nextIndex = i + 1
+            while nextIndex < dates.count, dates[nextIndex] == nil {
+                nextIndex += 1
+            }
+            
+            guard nextIndex < dates.count, let nextDate = dates[nextIndex] else {
+                return (i, nil)
+            }
+            
+            return (i, source.combinedValue(in: DateInterval(start: currentDate, end: nextDate)))
+        }
+        .filter { $0.1 != nil }
+        .map { DataPoint(x: Double($0.0), y: $0.1!) }
+    
+    let configure = { (config: ChartConfig) -> ChartConfig in
+        config.xAxisConfig.baseline = .zero
+        
+        if let barChartConfig = config as? BarChartConfig {
+            switch resolution {
+            case .day:
+                fallthrough
+            case .month:
+                barChartConfig.centerBars = false
+            default:
+                barChartConfig.centerBars = true
+            }
+        }
+        
+        switch resolution {
+        case .day:
+            config.xAxisConfig.step = .fixed(6)
+            config.xAxisConfig.scrollingBehaviour = .segmented(visibleValueRange: 4)
+            config.xAxisConfig.visibleValueRange = 24
+            config.xAxisConfig.topline = .clamp(lowerBound: 23)
+        case .week:
+            config.xAxisConfig.step = .fixed(1)
+            config.xAxisConfig.scrollingBehaviour = .segmented(visibleValueRange: 7)
+            config.xAxisConfig.visibleValueRange = 7
+            config.xAxisConfig.topline = .clamp(lowerBound: 6)
+        case .month:
+            config.xAxisConfig.step = .fixed(8)
+            config.xAxisConfig.scrollingBehaviour = .segmented(visibleValueRange: 4)
+            config.xAxisConfig.visibleValueRange = 32
+            config.xAxisConfig.topline = .clamp(lowerBound: 31)
+        case .threeMonths:
+            config.xAxisConfig.step = .fixed(1)
+            config.xAxisConfig.scrollingBehaviour = .segmented(visibleValueRange: 3)
+            config.xAxisConfig.visibleValueRange = 3
+            config.xAxisConfig.topline = .clamp(lowerBound: 2)
+        case .sixMonths:
+            config.xAxisConfig.step = .fixed(1)
+            config.xAxisConfig.scrollingBehaviour = .segmented(visibleValueRange: 6)
+            config.xAxisConfig.visibleValueRange = 6
+            config.xAxisConfig.topline = .clamp(lowerBound: 5)
+        case .year:
+            config.xAxisConfig.step = .fixed(1)
+            config.xAxisConfig.scrollingBehaviour = .segmented(visibleValueRange: 12)
+            config.xAxisConfig.visibleValueRange = 12
+            config.xAxisConfig.topline = .clamp(lowerBound: 11)
+        }
+        // FIXME: clone
+        return config
+    }
+    
+    return TimeSeriesData(scope: resolution, interval: valueInterval,
+                          dates: dates, values: values, configure: configure)
+}
+
 /// A chart wrapper that organizes data by time and passes it to an underlying chart implementation.
 ///
 /// Time series charts pull their data from a ``TimeSeriesDataSource``, which provides values for single dates
@@ -332,84 +410,6 @@ public struct TimeSeriesView<ChartContent: View>: View {
         self._scope = scope
     }
     
-    /// Create chart data for the currently selected time interval.
-    func getChartData(for resolution: TimeSeriesScope) -> TimeSeriesData {
-        let interval = source.interval
-        let (dates, valueInterval) = resolution.dates(in: interval)
-        let values = (0..<(dates.count-1))
-            .map { (i: Int) -> (Int, Double?) in
-                guard let currentDate = dates[i] else {
-                    return (i, nil)
-                }
-                
-                var nextIndex = i + 1
-                while nextIndex < dates.count, dates[nextIndex] == nil {
-                    nextIndex += 1
-                }
-                
-                guard nextIndex < dates.count, let nextDate = dates[nextIndex] else {
-                    return (i, nil)
-                }
-                
-                return (i, source.combinedValue(in: DateInterval(start: currentDate, end: nextDate)))
-            }
-            .filter { $0.1 != nil }
-            .map { DataPoint(x: Double($0.0), y: $0.1!) }
-        
-        let configure = { (config: ChartConfig) -> ChartConfig in
-            config.xAxisConfig.baseline = .zero
-            
-            if let barChartConfig = config as? BarChartConfig {
-                switch resolution {
-                case .day:
-                    fallthrough
-                case .month:
-                    barChartConfig.centerBars = false
-                default:
-                    barChartConfig.centerBars = true
-                }
-            }
-            
-            switch resolution {
-            case .day:
-                config.xAxisConfig.step = .fixed(6)
-                config.xAxisConfig.scrollingBehaviour = .segmented(visibleValueRange: 4)
-                config.xAxisConfig.visibleValueRange = 24
-                config.xAxisConfig.topline = .clamp(lowerBound: 23)
-            case .week:
-                config.xAxisConfig.step = .fixed(1)
-                config.xAxisConfig.scrollingBehaviour = .segmented(visibleValueRange: 7)
-                config.xAxisConfig.visibleValueRange = 7
-                config.xAxisConfig.topline = .clamp(lowerBound: 6)
-            case .month:
-                config.xAxisConfig.step = .fixed(8)
-                config.xAxisConfig.scrollingBehaviour = .segmented(visibleValueRange: 4)
-                config.xAxisConfig.visibleValueRange = 32
-                config.xAxisConfig.topline = .clamp(lowerBound: 31)
-            case .threeMonths:
-                config.xAxisConfig.step = .fixed(1)
-                config.xAxisConfig.scrollingBehaviour = .segmented(visibleValueRange: 3)
-                config.xAxisConfig.visibleValueRange = 3
-                config.xAxisConfig.topline = .clamp(lowerBound: 2)
-            case .sixMonths:
-                config.xAxisConfig.step = .fixed(1)
-                config.xAxisConfig.scrollingBehaviour = .segmented(visibleValueRange: 6)
-                config.xAxisConfig.visibleValueRange = 6
-                config.xAxisConfig.topline = .clamp(lowerBound: 5)
-            case .year:
-                config.xAxisConfig.step = .fixed(1)
-                config.xAxisConfig.scrollingBehaviour = .segmented(visibleValueRange: 12)
-                config.xAxisConfig.visibleValueRange = 12
-                config.xAxisConfig.topline = .clamp(lowerBound: 11)
-            }
-            // FIXME: clone
-            return config
-        }
-        
-        return TimeSeriesData(scope: resolution, interval: valueInterval,
-                              dates: dates, values: values, configure: configure)
-    }
-    
     public var body: some View {
         ZStack {
             if let data {
@@ -418,10 +418,91 @@ public struct TimeSeriesView<ChartContent: View>: View {
             }
         }
         .onAppear {
-            self.data = self.getChartData(for: scope)
+            self.data = getTimeSeriesChartData(source: source, for: scope)
         }
         .onChange(of: self.scope) { scope in
-            self.data = self.getChartData(for: scope)
+            self.data = getTimeSeriesChartData(source: source, for: scope)
+        }
+    }
+}
+
+/// A variant of `TimeSeriesView` that can handle mutiple data sources.
+///
+/// Time series charts pull their data from mutiplie ``TimeSeriesDataSource``, which provides values for single dates
+/// or date intervals. `TimeSeriesView` creates an instance of ``TimeSeriesData`` based on the selected scope
+/// as well as the data source and passes it to the chart builder closure to create a chart.
+///
+/// `TimeSeriesView` automatically handles data segmentation based on the selected scope. This means that any
+/// values you configure for the properties ``ChartAxisConfig/step``, ``ChartAxisConfig/scrollingBehaviour``,
+/// ``ChartAxisConfig/visibleValueRange``, and ``ChartAxisConfig/topline`` of the ``ChartAxisConfig``
+/// may be overriden to ensure a consistent appearance.
+///
+/// The following examples show what you can do with `TimeSeriesView`.
+///
+/// ![A time series using a bar chart with yearly scope](TimeSeriesBarYear)
+///
+/// The figure above shows a time series using a ``BarChart`` for visualization with the scope ``TimeSeriesScope/year``
+/// selected. The same chart would look like below if the scope is changed to ``TimeSeriesScope/month`` and ``TimeSeriesScope/week``,
+/// respectively.
+///
+/// ![A time series using a bar chart with monthly scope](TimeSeriesBarMonth)
+/// ![A time series using a bar chart with weekly scope](TimeSeriesBarWeek)
+///
+/// Any chart type can be used to create a time series. The next example shows one using a ``LineChart``.
+///
+/// ![A time series using a line chart with monthly scope](TimeSeriesLineMonth)
+///
+/// Automatic transitions between pages make it easier to visualize the differences in scale between different time intervals.
+///
+/// ![A time series with automatic transition animations](TimeSeriesTransitionMonth)
+/// ![A time series with automatic transition animations](TimeSeriesTransitionWeek)
+public struct MultipleTimeSeriesView<ChartContent: View>: View {
+    /// The chart data.
+    let sources: [TimeSeriesDataSource]
+    
+    /// The chart builder.
+    let buildChartView: ([TimeSeriesData]) -> ChartContent
+    
+    /// The current scope.
+    @Binding var scope: TimeSeriesScope
+    
+    /// The time series data.
+    @State var data: [TimeSeriesData]? = nil
+    
+    /// The chart state.
+    @State var chartState: ChartStateProxy? = nil
+    
+    /// The primary data source.
+    var primarySource: TimeSeriesDataSource { sources[0] }
+    
+    /// Create a time series chart.
+    ///
+    /// - Parameters:
+    ///   - source: The data source.
+    ///   - scope: Binding for the
+    ///   - buildChartView: Closure to build the chart from the current time series data.
+    public init(sources: [TimeSeriesDataSource],
+                scope: Binding<TimeSeriesScope>,
+                @ViewBuilder buildChartView: @escaping ([TimeSeriesData]) -> ChartContent) {
+        assert(!sources.isEmpty, "At least one data source must be provided.")
+        
+        self.sources = sources
+        self.buildChartView = buildChartView
+        self._scope = scope
+    }
+    
+    public var body: some View {
+        ZStack {
+            if let data {
+                self.buildChartView(data)
+                    .id(data.map(\.hashValue).hashValue)
+            }
+        }
+        .onAppear {
+            self.data = self.sources.map { getTimeSeriesChartData(source: $0, for: scope) }
+        }
+        .onChange(of: self.scope) { scope in
+            self.data = self.sources.map { getTimeSeriesChartData(source: $0, for: scope) }
         }
     }
 }
